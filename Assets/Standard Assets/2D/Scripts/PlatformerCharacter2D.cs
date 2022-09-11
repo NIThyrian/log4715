770680f85b5d22e88e5f4e5a70e4fb845173adc0
simon-mercier <simon-1.mercier@polymtl.ca>
2022-09-11 14:48:29 -0400

    added jump charging

--- Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -8,11 +8,15 @@ namespace UnityStandardAssets._2D
     {
         [SerializeField] private float m_MaxSpeed = 10f;                    // The fastest the player can travel in the x axis.
         [SerializeField] private float m_JumpForce = 400f;                  // Amount of force added when the player jumps.
-        [SerializeField] private uint m_MaxJumps = 3; //TODO: saut infini (gérer ce edge case)                    
-        [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;  // Amount of maxSpeed applied to crouching movement. 1 = 100%
+        [SerializeField] private float m_MaxChargedJumpForce = 400f;           // If the m_MaxChargedJumpForce is set to 0, then max jump force will be m_JumpForce
+        [SerializeField] private uint m_MaxJumps = 3; //TODO: saut infini (gï¿½rer ce edge case)                    
+        [Range(0, 1)][SerializeField] private float m_CrouchSpeed = .36f;  // Amount of maxSpeed applied to crouching movement. 1 = 100%
         [SerializeField] private bool m_AirControl = false;                 // Whether or not a player can steer while jumping;
         [SerializeField] private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
+
         private uint m_JumpCount = 0;
+        private float m_AccumulatedChargedJumpForce = 0f;
+        private readonly float m_ChargedJumpForceIncrement = 5f;
 
         private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
         const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
@@ -25,7 +29,10 @@ namespace UnityStandardAssets._2D
         private bool m_FacingRight = true;  // For determining which way the player is currently facing.
         private float m_WallTimer = 0f;
 
-        private int direction => m_FacingRight ? 1 : -1;
+        private const float k_ChargeTimerDefaultTime = 0.02f;
+        private float m_ChargeTimer = k_ChargeTimerDefaultTime;
+
+        private int Direction => m_FacingRight ? 1 : -1;
 
         private void Awake()
         {
@@ -55,15 +62,18 @@ namespace UnityStandardAssets._2D
             // Set the vertical animation
             m_Anim.SetFloat("vSpeed", m_Rigidbody2D.velocity.y);
 
-            RaycastHit2D[] hitWall = Physics2D.RaycastAll(transform.position, new Vector2(direction, 0), 1f);
-            if(hitWall.Length != 1) 
+            RaycastHit2D[] hitWall = Physics2D.RaycastAll(transform.position, new Vector2(Direction, 0), 1f);
+            if (hitWall.Length != 1)
             {
                 m_AgainstWall = true;
             }
+
+            m_ChargeTimer -= Time.fixedDeltaTime;
+
         }
 
 
-        public void Move(float move, bool crouch, bool jump)
+        public void Move(float move, bool crouch, bool jump, bool charging)
         {
             // If crouching, check to see if the character can stand up
             if (!crouch && m_Anim.GetBool("Crouch"))
@@ -82,27 +92,27 @@ namespace UnityStandardAssets._2D
             if (m_Grounded || m_AirControl)
             {
                 // Reduce the speed if crouching by the crouchSpeed multiplier
-                move = (crouch ? move*m_CrouchSpeed : move);
+                move = (crouch ? move * m_CrouchSpeed : move);
 
                 // The Speed animator parameter is set to the absolute value of the horizontal input.
                 m_Anim.SetFloat("Speed", Mathf.Abs(move));
 
                 // Move the character
-                if(m_WallTimer > 0) 
+                if (m_WallTimer > 0)
                 {
                     m_WallTimer -= Time.fixedDeltaTime;
                 }
-                else 
+                else
                 {
-                    m_Rigidbody2D.velocity = new Vector2(move*m_MaxSpeed, m_Rigidbody2D.velocity.y);
+                    m_Rigidbody2D.velocity = new Vector2(move * m_MaxSpeed, m_Rigidbody2D.velocity.y);
 
-                     // If the input is moving the player right and the player is facing left...
+                    // If the input is moving the player right and the player is facing left...
                     if (move > 0 && !m_FacingRight)
                     {
                         // ... flip the player.
                         Flip();
                     }
-                        // Otherwise if the input is moving the player left and the player is facing right...
+                    // Otherwise if the input is moving the player left and the player is facing right...
                     else if (move < 0 && m_FacingRight)
                     {
                         // ... flip the player.
@@ -111,15 +121,32 @@ namespace UnityStandardAssets._2D
                 }
 
             }
-            // If the player should jump...
-            if (jump)
+            if (charging)
             {
+                if (m_Grounded && m_ChargeTimer < 0)
+                {
+                    m_ChargeTimer = k_ChargeTimerDefaultTime;
+                    m_AccumulatedChargedJumpForce = Math.Min(m_MaxChargedJumpForce, m_AccumulatedChargedJumpForce + m_ChargedJumpForceIncrement);
+                    Debug.Log("charging");
+                }
+            }
+            else if (m_Grounded && m_AccumulatedChargedJumpForce > 0)
+            {
+                Jump(m_JumpForce + m_AccumulatedChargedJumpForce);
+                m_AccumulatedChargedJumpForce = 0;
+                Debug.Log("Releasing charge");
+            }
+            // If the player should jump or has accumulated some jump charge
+            else if (jump)
+            {
+
                 if (!m_Grounded && m_AgainstWall)
                 {
                     m_WallTimer = 0.7f;
                     Flip();
-                    Debug.Log(m_JumpForce * direction * -1);
-                    m_Rigidbody2D.AddForce(new Vector2(m_JumpForce * direction, m_JumpForce));
+                    Debug.Log(m_JumpForce * Direction * -1);
+                    m_Rigidbody2D.AddForce(new Vector2(m_JumpForce * Direction, m_JumpForce));
+                    Debug.Log("Jumping on wall");
                 }
                 else
                 {
@@ -130,13 +157,28 @@ namespace UnityStandardAssets._2D
                         m_Anim.SetBool("Ground", false);
                     }
 
-                    if(m_JumpCount++ <= m_MaxJumps)
+                    if (m_JumpCount++ <= m_MaxJumps)
                     {
                         // Add a vertical force to the player.
-                        m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+                        Jump(m_JumpForce);
+
+                        if (m_JumpCount-1 == 0)
+                        {
+                            Debug.Log("Jumping from ground");
+                        }
+                        else
+                        {
+                            Debug.Log($"Jumping in air {m_JumpCount-1}");
+                        }
                     }
                 }
+
             }
+        }
+
+        private void Jump( float force)
+        {
+            m_Rigidbody2D.AddForce(new Vector2(0f, force));
         }
 
 
